84607cd65fb7e971b8078d1717f34c34c5f7eed2
David Tran <david.tran@tvtmarine.com>
2018-04-27 21:59:11 +0700

    [MISC] web_responsive: auto_install is set True

--- web_responsive/__manifest__.py
+++ web_responsive/__manifest__.py
@@ -12,6 +12,7 @@
     "author": "LasLabs, Tecnativa, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "installable": True,
+    "auto_install": True,
     "depends": [
         'web',
     ],
